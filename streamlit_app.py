a263939fdedb184775390a38a71bde7b843a4691
stpeteishii <47345279+tztechno@users.noreply.github.com>
2024-11-26 16:05:17 +0900

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -32,14 +32,14 @@ if st.button("Predict Your Fortune"):
     times, fortune, name, images = predict()
     col1, col2, col3 = st.columns(3)
     with col1:
-        st.image(images[0], caption=name[0], use_column_width=True)
+        st.image(images[0], caption=name[0], use_container_width=True)
         st.write(f'Your {times[0]} is {name[0]}.')
         st.write(f'{fortune[0]}.')
     with col2:
-        st.image(images[1], caption=name[1], use_column_width=True)
+        st.image(images[1], caption=name[1], use_container_width=True)
         st.write(f'Your {times[1]} is {name[1]}.')
         st.write(f'{fortune[1]}.')
     with col3:
-        st.image(images[2], caption=name[2], use_column_width=True)
+        st.image(images[2], caption=name[2], use_container_width=True)
         st.write(f'Your {times[2]} is {name[2]}.')
         st.write(f'{fortune[2]}.')
